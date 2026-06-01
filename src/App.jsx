2a76f1741bf59e9f036a5a675a867e068c565b07
Parth Parmar <parthparmar271127@gmail.com>
2026-06-01 10:28:13 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        background: "#0f172a",
+        color: "white",
+        fontFamily: "Arial",
+      }}
+    >
+      <h1>My First CI/CD Project 🚀</h1>
+      <p>GitHub Actions + Vercel Deployment</p>
+
+      <button
+        style={{
+          padding: "12px 24px",
+          border: "none",
+          borderRadius: "10px",
+          background: "#38bdf8",
+          color: "black",
+          fontWeight: "bold",
+          cursor: "pointer",
+        }}
+      >
+        Live Project
+      </button>
+    </div>
+  );
+}
